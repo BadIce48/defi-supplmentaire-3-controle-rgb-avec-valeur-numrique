6c72100ab763e62e1f74ef37705c2e9a988c6de3
BadIce48 <116098979+BadIce48@users.noreply.github.com>
2022-11-03 14:31:22 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+let lumiere = 0
+let état_actuel = 0
+let état_passé = 0
 basic.forever(function () {
-	
+    état_actuel = pins.digitalReadPin(DigitalPin.P16)
+    if (état_actuel != état_passé && état_actuel == 1) {
+        lumiere += 100
+    }
+    if (lumiere == 1000) {
+        lumiere = 0
+    }
+})
+basic.forever(function () {
+    if (pins.analogReadPin(AnalogPin.P1) < 1000) {
+        pins.analogWritePin(AnalogPin.P1, lumiere)
+    } else if (pins.analogReadPin(AnalogPin.P1) == 1000) {
+        pins.analogWritePin(AnalogPin.P1, 0)
+        pins.analogWritePin(AnalogPin.P2, lumiere)
+    } else if (pins.analogReadPin(AnalogPin.P2) == 1000) {
+        pins.analogWritePin(AnalogPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P1, lumiere)
+        pins.analogWritePin(AnalogPin.P2, lumiere)
+        pins.analogWritePin(AnalogPin.P8, lumiere)
+    }
 })
